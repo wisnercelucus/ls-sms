d8c5fb209d569794e6c8bf62dd3c16e7047397f7
wisnercelucus <wisnercelicus@gmail.com>
2017-10-08 22:04:31 -0500

    upgrate view

--- school/views.py
+++ school/views.py
@@ -55,7 +55,7 @@ def import_data(request):
 			if settings.DEBUG == True:
 				data = csv.reader(open(doc.upload.path), delimiter=',')
 			else:
-				data = csv.reader(open(os.path.join(os.getcwd(), myfile.name)), delimiter=',')
+				data = csv.reader(open(os.path.join(os.getcwd(), myfile)), delimiter=',')
 				print(doc.upload.url)
 			header = next(data)
 			header_cols = convert_header(header)
